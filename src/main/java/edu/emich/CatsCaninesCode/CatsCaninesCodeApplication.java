d3123901fe776fa160145e977977ceeb2dcb53f6
Sam Keim <sblasko1@emich.edu>
2022-06-02 11:22:16 -0400

    Starter Spring Initalizr Project

---
+++ src/main/java/edu/emich/CatsCaninesCode/CatsCaninesCodeApplication.java
@@ -0,0 +1,13 @@
+package edu.emich.CatsCaninesCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsCaninesCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsCaninesCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/emich/CatsCaninesCode/CatsCaninesCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.emich.CatsCaninesCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsCaninesCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
